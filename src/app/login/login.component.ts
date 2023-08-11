aaee8054ba9bb1729858e9d15f30abaeaaded0cb
Navneet Sridhar <navneetsridhar17@gmail.com>
2023-08-11 11:16:20 +0530

    bank2

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  data = "- Happy Banking with us -"
+  // data2 ="Enter account number"
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  login(){
+    alert("login clicked")
+  }
+  acnoChange(event:any){
+    console.log(event.target.value);
+    
+  }
+
+  pswChange(event:any){
+    console.log(event.target.value);
+    
+  }
+
+}
